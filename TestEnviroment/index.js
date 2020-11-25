916ff4a7b168b4acba5eaf12516829bb62fb6118
Delmer López <delmrlophz@gmail.com>
2020-11-24 21:53:25 -0600

    Login added and hacked

---
+++ TestEnviroment/index.js
@@ -0,0 +1,60 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+app.use(express.static('client'));
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'auth'
+});
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname + '/client/index.html'));
+});
+
+// for action
+app.post('/login', function(request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    console.log(username, password);
+    if (username && password) {
+// check if user exists
+        connection.query(`SELECT * FROM users WHERE username = ${username} AND password = ${password}`, function(error, results, fields) {
+            if (results.length > 0) {
+                request.session.loggedin = true;
+                request.session.username = username;
+                response.redirect('/home');
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }           
+            response.end();
+        });
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
+});
+
+app.get('/dashboard', function(request, response) {
+    if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.username + '!');
+    } else {
+        response.send('Please login to view this page!');
+    }
+    response.end();
+});
+
+app.listen(3000);
